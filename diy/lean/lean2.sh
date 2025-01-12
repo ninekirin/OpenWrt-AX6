05b8b1e2dae1b2cf136177994367d60df1a4a87a
KirinBaka <bakaawt@hotmail.com>
2025-01-12 21:41:07 +0800

    fix fcnat

--- diy/lean/lean2.sh
+++ diy/lean/lean2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# fix fullcone nat
+# fix fullcone nat (must remove unnecessary includes for lean's lede)
 curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
 
 # translation fix
@@ -24,33 +24,22 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/luci2/bin/config_generat
 sed -i '/V4UetPzk$CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
 
 # set default theme to luci-theme-argon
-rm -rf ./feeds/luci/themes/luci-theme-argon
-rm -rf ./feeds/luci/themes/luci-theme-design
-rm -rf ./feeds/luci/themes/luci-theme-argon-mod
+# rm -rf ./feeds/luci/themes/luci-theme-argon
+# rm -rf ./feeds/luci/themes/luci-theme-design
+# rm -rf ./feeds/luci/themes/luci-theme-argon-mod
 
-rm -rf ./package/feeds/luci/luci-theme-argon
-rm -rf ./package/feeds/luci/luci-theme-design
-rm -rf ./package/feeds/luci/luci-theme-argon-mod
+# rm -rf ./package/feeds/luci/luci-theme-argon
+# rm -rf ./package/feeds/luci/luci-theme-design
+# rm -rf ./package/feeds/luci/luci-theme-argon-mod
 
-sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-nginx/Makefile
+# sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-nginx/Makefile
 
 # 加入作者信息
 sed -i "s/DISTRIB_DESCRIPTION='*.*'/DISTRIB_DESCRIPTION='OpenWrt-$(date +%Y%m%d)'/g" package/lean/default-settings/files/zzz-default-settings   
 sed -i "s/DISTRIB_REVISION='*.*'/DISTRIB_REVISION=' by Kirin'/g" package/lean/default-settings/files/zzz-default-settings
-cp -af feeds/2305ipk/patch/diy/banner package/base-files/files/etc/banner
 
 # 更改主机名
 sed -i "s/hostname='.*'/hostname='OpenWrt'/g" package/base-files/files/bin/config_generate
 sed -i "s/hostname='.*'/hostname='OpenWrt'/g" package/base-files/luci2/bin/config_generate
-
-# WiFi SSID
-sed -i "s/LEDE/KirinWLAN/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
-
-## Remove Interface Line of Dropbear
-sed -i '/option Interface/d' package/network/services/dropbear/files/dropbear.config
-
-## golang 为 1.23.x
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
